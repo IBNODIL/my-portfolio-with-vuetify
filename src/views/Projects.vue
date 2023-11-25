6a08df33f2fe6313277cce3b6f4c4b49cab10bd2
IBNODIL <ibnodil2010@gmail.com>
2023-11-25 16:52:02 +0300

    add new projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -17,7 +17,37 @@
     </v-card>
 
     <v-card class="ma-6 pa-0">
-      <a href="https://weather-app-with-vue-ibn-odil-dev.netlify.app/" target="_blank">
+      <a href="https://akadem-nashr-ibn-odil-dev.netlify.app/" target="_blank">
+        <img src="../assets/projects-img/akadem-nashr.png">
+      </a>
+      <v-card-title>
+        Akadem nashr
+      </v-card-title>
+      <v-card-subtitle>
+        <p>
+          <v-icon>mdi-language-html5</v-icon>
+          <v-icon>mdi-language-css3</v-icon>
+        </p>
+      </v-card-subtitle>
+    </v-card>
+
+    <v-card class="ma-6 pa-0">
+      <a href="https://ansor-med-ibn-odil-dev.netlify.app/" target="_blank">
+        <img src="../assets/projects-img/ansor-med.png">
+      </a>
+      <v-card-title>
+        Ansormed
+      </v-card-title>
+      <v-card-subtitle>
+        <p>
+          <v-icon>mdi-language-html5</v-icon>
+          <v-icon>mdi-language-css3</v-icon>
+        </p>
+      </v-card-subtitle>
+    </v-card>
+    
+    <v-card class="ma-6 pa-0">
+      <a href="https://parallax-web-site-ibn-odil-dev.netlify.app/" target="_blank">
         <img src="../assets/projects-img/parallax.jpg">
       </a>
       <v-card-title>
